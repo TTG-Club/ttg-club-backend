cddbf8e45f6e2f83e2f793a7c7c966c04961df72
svifty7 <svifty7@gmail.com>
2022-11-05 21:03:47 +0300

    fix deploy commit sha

--- src/main/java/club/dnd5/portal/config/VersionControllerAdvice.java
+++ src/main/java/club/dnd5/portal/config/VersionControllerAdvice.java
@@ -8,20 +8,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 @ControllerAdvice
 public class VersionControllerAdvice {
-	@Value("${app.frontend.sha}")
-	private String version;
-
 	@Value("${spring.profiles.active}")
 	private String profile;
 
 	@ModelAttribute
 	public void handleRequest(HttpServletRequest request, Model model) {
-		model.addAttribute("version", version);
+		model.addAttribute("version", getVersion());
 		model.addAttribute("profile", profile);
 
 		String themeName = "dark";
@@ -36,5 +34,21 @@ public class VersionControllerAdvice {
 		}
 
 		model.addAttribute("themeName", themeName);
+	}
+
+	private String getVersion() {
+		try {
+			InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("my.properties");
+			Properties properties = new Properties();
+			properties.load(inputStream);
+
+			if (properties.getProperty("app.frontend.sha") != null) {
+				return properties.getProperty("app.frontend.sha");
+			}
+
+			return "";
+		} catch (IOException err) {
+			return "";
+		}
 	}
 }
