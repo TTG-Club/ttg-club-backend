fa790bcab6a5f5dc82b22000120bc33f797c0ec9
Magistrus <gm.magistr@gmail.com>
2022-11-12 01:06:27 +0200

    fix meta URL

--- src/main/java/club/dnd5/portal/controller/ItemController.java
+++ src/main/java/club/dnd5/portal/controller/ItemController.java
@@ -17,18 +17,20 @@ import io.swagger.v3.oas.annotations.Hidden;
 @Hidden
 @Controller
 public class ItemController {
+	private static final String BASE_URL = "https://ttg.club/items";
+
 	@Autowired
 	private ItemDatatableRepository repository;
-	
+
 	@GetMapping("/items")
 	public String getItems(Model model) {
 		model.addAttribute("metaTitle", "Снаряжение (Items) D&D 5e");
-		model.addAttribute("metaUrl", "https://ttg.club/items");
+		model.addAttribute("metaUrl", BASE_URL);
 		model.addAttribute("metaDescription", "Снаряжение и инструменты по D&D 5 редакции");
 		model.addAttribute("menuTitle", "Снаряжение");
 		return "items";
 	}
-	
+
 	@GetMapping("/items/{name}")
 	public String getItem(Model model, @PathVariable String name, HttpServletRequest request) {
 		Equipment item = repository.findByEnglishName(name.replace("_", " "));
@@ -37,20 +39,20 @@ public class ItemController {
 			return "forward: /error";
 		}
 		model.addAttribute("metaTitle", item.getName() + " | Снаряжение D&D 5e");
-		model.addAttribute("metaUrl", "https://ttg.club/items/" + name);
+		model.addAttribute("metaUrl", String.format("%s/%s", BASE_URL, name));
 		model.addAttribute("metaDescription", String.format("%s (%s) снаряжение по D&D 5 редакции", item.getName(), item.getEnglishName()));
 		model.addAttribute("menuTitle", "Снаряжение");
 		return "items";
 	}
-	
+
 	@GetMapping("/items/fragment/{id:\\d+}")
 	public String getMagicItemFragmentById(Model model, @PathVariable Integer id) throws InvalidAttributesException {
 		model.addAttribute("item", repository.findById(id).orElseThrow(InvalidAttributesException::new));
 		return "fragments/item :: view";
 	}
-	
+
 	@GetMapping("/items/fragment/{name:[A-Za-z_,']+}")
-	public String getMagicWeaponFragmentByName(Model model, @PathVariable String name) throws InvalidAttributesException {
+	public String getMagicWeaponFragmentByName(Model model, @PathVariable String name) {
 		model.addAttribute("item", repository.findByEnglishName(name.replace('_', ' ')));
 		return "fragments/item :: view";
 	}
