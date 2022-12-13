d8bcd9a3eb983b3186878fbe79d614035348fdde
Magistrus <gm.magistr@gmail.com>
2022-12-13 18:43:25 +0200

    full text search

--- src/main/java/club/dnd5/portal/controller/api/FullSearchController.java
+++ src/main/java/club/dnd5/portal/controller/api/FullSearchController.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+import club.dnd5.portal.repository.datatable.BackgroundDatatableRepository;
+import club.dnd5.portal.repository.datatable.OptionDatatableRepository;
+import club.dnd5.portal.repository.datatable.SpellDatatableRepository;
 import club.dnd5.portal.repository.datatable.TraitDatatableRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 import club.dnd5.portal.dto.api.SearchApi;
 import club.dnd5.portal.repository.classes.ClassRepository;
 import club.dnd5.portal.repository.classes.RaceRepository;
-import io.swagger.v3.oas.annotations.Hidden;
 
-@Tag(name = "Global search", description = "The search API")
+@Tag(name = "Full search", description = "The search API")
 @RestController
 @RequestMapping("/api/v1/search")
-public class GlobalSearchController {
+public class FullSearchController {
 	@Autowired
 	ClassRepository classRepository;
 
@@ -27,6 +29,12 @@ public class GlobalSearchController {
 	RaceRepository raceRepository;
 	@Autowired
 	TraitDatatableRepository traitRepository;
+	@Autowired
+	OptionDatatableRepository optionRepository;
+	@Autowired
+	BackgroundDatatableRepository backgroundRepository;
+	@Autowired
+	SpellDatatableRepository spellRepository;
 
 	@PostMapping
 	public Collection<SearchApi> search(String search){
@@ -36,6 +44,18 @@ public class GlobalSearchController {
 				.map(SearchApi::new)
 				.collect(Collectors.toList()));
 		result.addAll(traitRepository.findByEnglishNameContainsOrNameContainsOrAltNameContains(search, search, search)
+			.stream()
+			.map(SearchApi::new)
+			.collect(Collectors.toList()));
+		result.addAll(optionRepository.findByEnglishNameContainsOrNameContainsOrAltNameContains(search, search, search)
+			.stream()
+			.map(SearchApi::new)
+			.collect(Collectors.toList()));
+		result.addAll(backgroundRepository.findByEnglishNameContainsOrNameContainsOrAltNameContains(search, search, search)
+			.stream()
+			.map(SearchApi::new)
+			.collect(Collectors.toList()));
+		result.addAll(spellRepository.findByEnglishNameContainsOrNameContainsOrAltNameContains(search, search, search)
 			.stream()
 			.map(SearchApi::new)
 			.collect(Collectors.toList()));
--- src/main/java/club/dnd5/portal/dto/api/SearchApi.java
+++ src/main/java/club/dnd5/portal/dto/api/SearchApi.java
@@ -1,5 +1,8 @@
 package club.dnd5.portal.dto.api;
 
+import club.dnd5.portal.model.background.Background;
+import club.dnd5.portal.model.classes.Option;
+import club.dnd5.portal.model.splells.Spell;
 import club.dnd5.portal.model.trait.Trait;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -27,5 +30,20 @@ public class SearchApi {
 		name = new NameApi(trait.getName(), trait.getEnglishName());
 		section = "Черты";
 		url = String.format("/traits/%s", trait.getUrlName());
+	}
+	public SearchApi(Option option) {
+		name = new NameApi(option.getName(), option.getEnglishName());
+		section = "Особенности классов";
+		url = String.format("/traits/%s", option.getUrlName());
+	}
+	public SearchApi(Background background) {
+		name = new NameApi(background.getName(), background.getEnglishName());
+		section = "Особенности классов";
+		url = String.format("/backgrounds/%s", background.getUrlName());
+	}
+	public SearchApi(Spell spell) {
+		name = new NameApi(spell.getName(), spell.getEnglishName());
+		section = "Особенности классов";
+		url = String.format("/spells/%s", spell.getUrlName());
 	}
 }
