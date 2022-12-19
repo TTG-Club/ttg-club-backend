df0ebeaa634de7de7706f42dbac1db0af4a754af
svifty7 <svifty7@gmail.com>
2022-12-19 13:21:42 +0300

    review fixes

--- src/main/java/club/dnd5/portal/model/Menu.java
+++ src/main/java/club/dnd5/portal/model/Menu.java
@@ -14,20 +14,24 @@ public class Menu {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	@Basic(optional = false)
 	@Column(nullable = false)
 	private String name;
 	private String icon;
 	private String url;
+	private Boolean external;
 	@JoinColumn(name = "in_dev")
 	private Boolean inDev;
+
 	@Basic(optional = false)
 	@Column(nullable = false)
 	private Integer order;
+
 	@ManyToOne
 	@JoinColumn(name = "parent_id")
 	private Menu parent;
+
 	@OneToMany(mappedBy = "parent", orphanRemoval = false, cascade = CascadeType.REMOVE)
 	private List<Menu> children;
-	private Boolean external;
 }
