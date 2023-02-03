6e0f6a19557650ead40f5c67100fa053d5789e1b
Magistrus <gm.magistr@gmail.com>
2023-02-03 09:55:08 +0200

    full search refactoring to sql view

--- src/main/java/club/dnd5/portal/repository/SearchRepository.java
+++ src/main/java/club/dnd5/portal/repository/SearchRepository.java
@@ -15,44 +15,8 @@ import java.util.stream.Collectors;
 
 @Repository
 public class SearchRepository {
-	private String SQL_SEARCH;
-
 	@PersistenceContext
 	EntityManager entityManager;
-
-	@PostConstruct
-	public void init(){
-		SQL_SEARCH =
-				"SELECT name, 'Заклинания' as section, CONCAT('/spells/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM spells WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name" +
-				" UNION ALL " +
-				"SELECT name, 'Классы' as section, CONCAT('/classes/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM classes WHERE name LIKE :name OR english_name LIKE :name" +
-				" UNION ALL " +
-				"SELECT a.name, 'Архетипы классов' as section, CONCAT('/classes/', REPLACE(LOWER(c.english_name), ' ', '_'), '/', REPLACE(LOWER(a.english_name), ' ', '_')) url, a.description FROM archetypes a JOIN classes c ON c.id = a.class_id WHERE a.name LIKE :name OR a.english_name LIKE :name" +
-				" UNION ALL " +
-				"SELECT r.name, 'Расы и происхождения ' as section, CASE WHEN p.name IS NOT NULL THEN CONCAT('/races/', REPLACE(LOWER(p.english_name), ' ', '_'), '/',REPLACE(LOWER(r.english_name), ' ', '_')) ELSE CONCAT('/races/', REPLACE(LOWER(r.english_name), ' ', '_1')) END AS url, r.description FROM races r LEFT JOIN races p ON r.parent_id = p.id WHERE r.name LIKE :name OR r.alt_name LIKE :name OR r.english_name LIKE :name" +
-				"  UNION ALL " +
-				"SELECT name, 'Бестиарий' as section, CONCAT('/bestiary/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM creatures WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name" +
-				"  UNION ALL " +
-				"SELECT name, 'Магические предметы' as section, CONCAT('/items/magic/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM artifactes WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name" +
-				"  UNION ALL " +
-				"SELECT name, 'Снаряжение' as section, CONCAT('/items/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM equipments WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name" +
-				"  UNION ALL " +
-				"SELECT name, 'Оружие' as section, CONCAT('/weapons/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM weapons WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name" +
-				"  UNION ALL " +
-				"SELECT name, 'Доспехи' as section, CONCAT('/armors/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM armors WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name" +
-				" UNION ALL " +
-				"SELECT name, 'Ширма Мастера' as section, CONCAT('/screens/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM screens WHERE name LIKE :name OR english_name LIKE :name OR alt_name LIKE :name" +
-				" UNION ALL " +
-				"SELECT name, 'Правила и термины' as section, CONCAT('/rules/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM rules WHERE name LIKE :name OR english_name LIKE :name" +
-				" UNION ALL " +
-				"SELECT name, 'Черты' as section, CONCAT('/traits/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM traits WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name" +
-				" UNION ALL " +
-				"SELECT name, 'Особенности классов' as section, CONCAT('/options/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM options WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name" +
-				" UNION ALL " +
-				"SELECT name, 'Предыстории и происхождения' as section, CONCAT('/backgrounds/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM backgrounds WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name"+
-				" UNION ALL " +
-				"SELECT name, 'Боги' as section, CONCAT('/gods/', REPLACE(LOWER(english_name), ' ', '_')) url, description FROM gods WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name";
-	}
 
 	public long getCount(String searchText) {
 		Query query = entityManager.createNativeQuery("SELECT COUNT(*) FROM full_text_search WHERE name LIKE :name OR alt_name LIKE :name OR english_name LIKE :name");
