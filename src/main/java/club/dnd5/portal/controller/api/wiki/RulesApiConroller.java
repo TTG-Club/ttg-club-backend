426565da4f15e39491e60cbee601aae336b5d5f2
svifty7 <svifty7@gmail.com>
2023-02-19 02:21:39 +0300

    fix rules

--- src/main/java/club/dnd5/portal/controller/api/wiki/RulesApiConroller.java
+++ src/main/java/club/dnd5/portal/controller/api/wiki/RulesApiConroller.java
@@ -1,11 +1,14 @@
 package club.dnd5.portal.controller.api.wiki;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.persistence.criteria.Order;
-
+import club.dnd5.portal.dto.api.FilterApi;
+import club.dnd5.portal.dto.api.FilterValueApi;
+import club.dnd5.portal.dto.api.wiki.RuleApi;
+import club.dnd5.portal.dto.api.wiki.RuleDetailApi;
+import club.dnd5.portal.dto.api.wiki.RuleRequestApi;
+import club.dnd5.portal.model.rule.Rule;
+import club.dnd5.portal.repository.datatable.RuleDatatableRepository;
+import club.dnd5.portal.util.SpecificationUtil;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.datatables.mapping.Column;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
@@ -18,22 +21,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import club.dnd5.portal.dto.api.FilterApi;
-import club.dnd5.portal.dto.api.FilterValueApi;
-import club.dnd5.portal.dto.api.wiki.RuleApi;
-import club.dnd5.portal.dto.api.wiki.RuleDetailApi;
-import club.dnd5.portal.dto.api.wiki.RuleRequestApi;
-import club.dnd5.portal.model.rule.Rule;
-import club.dnd5.portal.repository.datatable.RuleDatatableRepository;
-import club.dnd5.portal.util.SpecificationUtil;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.persistence.criteria.Order;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Tag(name = "Rule", description = "The Rule API")
 @RestController
 public class RulesApiConroller {
 	@Autowired
 	private RuleDatatableRepository ruleRepository;
-	
+
 	@PostMapping(value = "/api/v1/rules", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<RuleApi> getRules(@RequestBody RuleRequestApi request) {
 		Specification<Rule> specification = null;
@@ -47,7 +45,7 @@ public class RulesApiConroller {
 		column.setOrderable(Boolean.TRUE);
 		column.setSearch(new Search("", Boolean.FALSE));
 		columns.add(column);
-		
+
 		column = new Column();
 		column.setData("englishName");
 		column.setName("englishName");
@@ -55,7 +53,7 @@ public class RulesApiConroller {
 		column.setSearchable(Boolean.TRUE);
 		column.setOrderable(Boolean.TRUE);
 		columns.add(column);
-		
+
 		column = new Column();
 		column.setData("altName");
 		column.setName("altName");
@@ -77,7 +75,7 @@ public class RulesApiConroller {
 		input.setColumns(columns);
 		input.setLength(request.getLimit() != null ? request.getLimit() : -1);
 		if (request.getPage() != null && request.getLimit()!=null) {
-			input.setStart(request.getPage() * request.getLimit());	
+			input.setStart(request.getPage() * request.getLimit());
 		}
 		if (request.getSearch() != null) {
 			if (request.getSearch().getValue() != null && !request.getSearch().getValue().isEmpty()) {
@@ -92,12 +90,12 @@ public class RulesApiConroller {
 		if (request.getFilter() != null) {
 			if (!request.getFilter().getCategory().isEmpty()) {
 				specification = SpecificationUtil.getAndSpecification(
-					specification, (root, query, cb) -> root.get("type").in(request.getFilter().getCategory()));
+					specification, (root, query, cb) -> root.get("category").in(request.getFilter().getCategory()));
 			}
 		}
 		return ruleRepository.findAll(input, specification, specification, RuleApi::new).getData();
 	}
-	
+
 	@PostMapping(value = "/api/v1/rules/{englishName}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<RuleDetailApi> getRule(@PathVariable String englishName) {
 		Rule rule = ruleRepository.findByEnglishName(englishName.replace('_', ' '));
@@ -106,13 +104,13 @@ public class RulesApiConroller {
 		}
 		return ResponseEntity.ok(new RuleDetailApi(rule));
 	}
-	
+
 	@PostMapping("/api/v1/filters/rules")
 	public FilterApi getFilter() {
 		FilterApi filters = new FilterApi();
 
 		List<FilterApi> otherFilters = new ArrayList<>();
-		
+
 		FilterApi categoryFilter = new FilterApi("Категория", "category");
 		categoryFilter.setValues(
 				ruleRepository.findAllCategories().stream()
