c34301aa216acbd4416dcadb308513ed34c4f09f
Magistrus <gm.magistr@gmail.com>
2023-02-18 23:57:34 +0200

    add bestiary filters

--- src/main/java/club/dnd5/portal/controller/api/bestiary/BestiarytApiConroller.java
+++ src/main/java/club/dnd5/portal/controller/api/bestiary/BestiarytApiConroller.java
@@ -115,134 +115,132 @@ public class BestiarytApiConroller {
 			}
 		}
 		Optional<BeastFilter> filter = Optional.ofNullable(request.getFilter());
-		if (request.getFilter() != null) {
-			if (!filter.map(BeastFilter::getNpc).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> cb.notEqual(root.get("raceId"), 102));
+		if (!filter.map(BeastFilter::getNpc).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> cb.notEqual(root.get("raceId"), 102));
+		}
+		if (!filter.map(BeastFilter::getChallengeRatings).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> root.get("challengeRating").in(request.getFilter().getChallengeRatings()));
+		}
+		if (!filter.map(BeastFilter::getTypes).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(
+				specification, (root, query, cb) -> root.get("type").in(request.getFilter().getTypes()));
+		}
+		if (!filter.map(BeastFilter::getSizes).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(
+				specification, (root, query, cb) -> root.get("size").in(request.getFilter().getSizes()));
+		}
+		if (!filter.map(BeastFilter::getTags).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> {
+				Join<Object, Object> join = root.join("races", JoinType.INNER);
+				query.distinct(true);
+				return cb.and(join.get("id").in(request.getFilter().getTags()));
+			});
+		}
+		if (!filter.map(BeastFilter::getBooks).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> {
+				Join<Book, Spell> join = root.join("book", JoinType.INNER);
+				return join.get("source").in(request.getFilter().getBooks());
+			});
+		}
+		if (!filter.map(BeastFilter::getMoving).orElseGet(Collections::emptyList).isEmpty()) {
+			if (request.getFilter().getMoving().contains("fly")) {
+				specification = SpecificationUtil.getAndSpecification(specification,
+					(root, query, cb) -> cb.isNotNull(root.get("flySpeed")));
 			}
-			if (!filter.map(BeastFilter::getChallengeRatings).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> root.get("challengeRating").in(request.getFilter().getChallengeRatings()));
+			if (request.getFilter().getMoving().contains("hover")) {
+				specification = SpecificationUtil.getAndSpecification(specification,
+					(root, query, cb) -> cb.isNotNull(root.get("hover")));
 			}
-			if (!filter.map(BeastFilter::getTypes).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(
-						specification,  (root, query, cb) -> root.get("type").in(request.getFilter().getTypes()));
+			if (request.getFilter().getMoving().contains("climbs")) {
+				specification = SpecificationUtil.getAndSpecification(specification,
+					(root, query, cb) -> cb.isNotNull(root.get("climbingSpeed")));
 			}
-			if (!filter.map(BeastFilter::getSizes).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(
-						specification,  (root, query, cb) -> root.get("size").in(request.getFilter().getSizes()));
+			if (request.getFilter().getMoving().contains("swim")) {
+				specification = SpecificationUtil.getAndSpecification(specification,
+					(root, query, cb) -> cb.isNotNull(root.get("swimmingSpped")));
 			}
-			if (!filter.map(BeastFilter::getTags).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(specification,  (root, query, cb) -> {
-					Join<Object, Object> join = root.join("races", JoinType.INNER);
+			if (request.getFilter().getMoving().contains("digger")) {
+				specification = SpecificationUtil.getAndSpecification(specification,
+					(root, query, cb) -> cb.isNotNull(root.get("diggingSpeed")));
+			}
+		}
+		if (!filter.map(BeastFilter::getEnvironments).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> {
+				Join<Object, Object> join = root.join("habitates", JoinType.INNER);
+				query.distinct(true);
+				return join.in(request.getFilter().getEnvironments());
+			});
+		}
+		if (!filter.map(BeastFilter::getVulnerabilityDamage).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> {
+				Join<Object, Object> join = root.join("vulnerabilityDamages", JoinType.INNER);
+				query.distinct(true);
+				return join.in(request.getFilter().getVulnerabilityDamage());
+			});
+		}
+		if (!filter.map(BeastFilter::getResistanceDamage).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> {
+				Join<Object, Object> join = root.join("resistanceDamages", JoinType.INNER);
+				query.distinct(true);
+				return join.in(request.getFilter().getResistanceDamage());
+			});
+		}
+		if (!filter.map(BeastFilter::getImmunityDamage).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> {
+				Join<Object, Object> join = root.join("immunityCondition", JoinType.INNER);
+				query.distinct(true);
+				return join.in(request.getFilter().getImmunityDamage());
+			});
+		}
+		if (!filter.map(BeastFilter::getImmunityCondition).orElseGet(Collections::emptyList).isEmpty()) {
+			specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> {
+				Join<Object, Object> join = root.join("immunityStates", JoinType.INNER);
+				query.distinct(true);
+				return join.in(request.getFilter().getImmunityCondition());
+			});
+		}
+		if (!filter.map(BeastFilter::getSenses).orElseGet(Collections::emptyList).isEmpty()) {
+			if (request.getFilter().getSenses().contains("darkvision")) {
+				specification = SpecificationUtil.getAndSpecification(specification,
+					(root, query, cb) -> cb.isNotNull(root.get("darkvision")));
+			}
+			if (request.getFilter().getSenses().contains("trysight")) {
+				specification = SpecificationUtil.getAndSpecification(specification,
+					(root, query, cb) -> cb.isNotNull(root.get("trysight")));
+			}
+			if (request.getFilter().getSenses().contains("blindsight")) {
+				specification = SpecificationUtil.getAndSpecification(specification,
+					(root, query, cb) -> cb.isNotNull(root.get("blindsight")));
+			}
+			if (request.getFilter().getSenses().contains("tremmor")) {
+				specification = SpecificationUtil.getAndSpecification(specification,
+					(root, query, cb) -> cb.isNotNull(root.get("vibration")));
+			}
+		}
+		if (!filter.map(BeastFilter::getFeatures).orElseGet(Collections::emptyList).isEmpty()) {
+			Specification<Creature> addSpec = null;
+			if (request.getFilter().getFeatures().contains("lair")) {
+				addSpec = SpecificationUtil.getOrSpecification(null, (root, query, cb) ->
+					cb.isNotNull(root.get("lair"))
+				);
+				request.getFilter().getFeatures().remove("lair");
+			}
+			if (request.getFilter().getFeatures().contains("legendary")) {
+				addSpec = SpecificationUtil.getOrSpecification(addSpec, (root, query, cb) -> {
+					Join<Action, Creature> join = root.join("actions", JoinType.INNER);
 					query.distinct(true);
-					return cb.and(join.get("id").in(request.getFilter().getTags()));
+					return cb.equal(join.get("actionType"), ActionType.LEGENDARY);
 				});
+				request.getFilter().getFeatures().remove("legendary");
 			}
-			if (!filter.map(BeastFilter::getBooks).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> {
-					Join<Book, Spell> join = root.join("book", JoinType.INNER);
-					return join.get("source").in(request.getFilter().getBooks());
-				});
-			}
-			if (!filter.map(BeastFilter::getMoving).orElseGet(Collections::emptyList).isEmpty()) {
-				if(request.getFilter().getMoving().contains("fly")) {
-					specification = SpecificationUtil.getAndSpecification(specification,
-							(root, query, cb) -> cb.isNotNull(root.get("flySpeed")));
-				}
-				if(request.getFilter().getMoving().contains("hover")) {
-					specification = SpecificationUtil.getAndSpecification(specification,
-							(root, query, cb) -> cb.isNotNull(root.get("hover")));
-				}
-				if(request.getFilter().getMoving().contains("climbs")) {
-					specification = SpecificationUtil.getAndSpecification(specification,
-							(root, query, cb) -> cb.isNotNull(root.get("climbingSpeed")));
-				}
-				if(request.getFilter().getMoving().contains("swim")) {
-					specification = SpecificationUtil.getAndSpecification(specification,
-							(root, query, cb) -> cb.isNotNull(root.get("swimmingSpped")));
-				}
-				if(request.getFilter().getMoving().contains("digger")) {
-					specification = SpecificationUtil.getAndSpecification(specification,
-							(root, query, cb) -> cb.isNotNull(root.get("diggingSpeed")));
-				}
-			}
-			if (!filter.map(BeastFilter::getEnvironments).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(specification,  (root, query, cb) -> {
-					Join<Object, Object> join = root.join("habitates", JoinType.INNER);
+			for (String featureName : request.getFilter().getFeatures()) {
+				addSpec = SpecificationUtil.getOrSpecification(addSpec, (root, query, cb) -> {
+					Join<CreatureFeat, Creature> join = root.join("feats", JoinType.INNER);
 					query.distinct(true);
-					return join.in(request.getFilter().getEnvironments());
+					return cb.like(join.get("name"), "%" + featureName + "%");
 				});
 			}
-			if (!filter.map(BeastFilter::getVulnerabilityDamage).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(specification,  (root, query, cb) -> {
-					Join<Object, Object> join = root.join("vulnerabilityDamages", JoinType.INNER);
-					query.distinct(true);
-					return join.in(request.getFilter().getVulnerabilityDamage());
-				});
-			}
-			if (!filter.map(BeastFilter::getResistanceDamage).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(specification,  (root, query, cb) -> {
-					Join<Object, Object> join = root.join("resistanceDamages", JoinType.INNER);
-					query.distinct(true);
-					return join.in(request.getFilter().getResistanceDamage());
-				});
-			}
-			if (!filter.map(BeastFilter::getImmunityDamage).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(specification,  (root, query, cb) -> {
-					Join<Object, Object> join = root.join("immunityCondition", JoinType.INNER);
-					query.distinct(true);
-					return join.in(request.getFilter().getImmunityDamage());
-				});
-			}
-			if (!filter.map(BeastFilter::getImmunityCondition).orElseGet(Collections::emptyList).isEmpty()) {
-				specification = SpecificationUtil.getAndSpecification(specification,  (root, query, cb) -> {
-					Join<Object, Object> join = root.join("immunityStates", JoinType.INNER);
-					query.distinct(true);
-					return join.in(request.getFilter().getImmunityCondition());
-				});
-			}
-			if (!filter.map(BeastFilter::getSenses).orElseGet(Collections::emptyList).isEmpty()) {
-				if(request.getFilter().getSenses().contains("darkvision")) {
-					specification = SpecificationUtil.getAndSpecification(specification,
-							(root, query, cb) -> cb.isNotNull(root.get("darkvision")));
-				}
-				if(request.getFilter().getSenses().contains("trysight")) {
-					specification = SpecificationUtil.getAndSpecification(specification,
-							(root, query, cb) -> cb.isNotNull(root.get("trysight")));
-				}
-				if(request.getFilter().getSenses().contains("blindsight")) {
-					specification = SpecificationUtil.getAndSpecification(specification,
-							(root, query, cb) -> cb.isNotNull(root.get("blindsight")));
-				}
-				if(request.getFilter().getSenses().contains("tremmor")) {
-					specification = SpecificationUtil.getAndSpecification(specification,
-							(root, query, cb) -> cb.isNotNull(root.get("vibration")));
-				}
-			}
-			if (!filter.map(BeastFilter::getFeatures).orElseGet(Collections::emptyList).isEmpty()) {
-				Specification<Creature> addSpec = null;
-				if (request.getFilter().getFeatures().contains("lair")) {
-					addSpec = SpecificationUtil.getOrSpecification(addSpec,  (root, query, cb) ->
-						cb.isNotNull(root.get("lair"))
-					);
-					request.getFilter().getFeatures().remove("lair");
-				}
-				if (request.getFilter().getFeatures().contains("legendary")) {
-					addSpec = SpecificationUtil.getOrSpecification(addSpec,  (root, query, cb) -> {
-						Join<Action, Creature> join = root.join("actions", JoinType.INNER);
-						query.distinct(true);
-						return cb.equal(join.get("actionType"), ActionType.LEGENDARY);
-					});
-					request.getFilter().getFeatures().remove("legendary");
-				}
-				for (String featureName : request.getFilter().getFeatures()) {
-					addSpec = SpecificationUtil.getOrSpecification(addSpec,  (root, query, cb) -> {
-						Join<CreatureFeat, Creature> join = root.join("feats", JoinType.INNER);
-						query.distinct(true);
-						return cb.like(join.get("name"), "%" + featureName + "%");
-					});
-				}
-				specification = SpecificationUtil.getAndSpecification(specification, addSpec);
-			}
+			specification = SpecificationUtil.getAndSpecification(specification, addSpec);
 		}
 		return beastRepository.findAll(input, specification, specification, BeastApi::new).getData();
 	}
