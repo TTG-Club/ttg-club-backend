fff1c4940b2163c3836b093652a96c8afb26e943
svifty7 <svifty7@gmail.com>
2023-02-21 04:25:47 +0300

    temp fix

--- src/main/java/club/dnd5/portal/util/NotHandlerControllerAdvice.java
+++ src/main/java/club/dnd5/portal/util/NotHandlerControllerAdvice.java
@@ -1,24 +1,24 @@
 package club.dnd5.portal.util;
 
-import club.dnd5.portal.config.VersionControllerAdvice;
 import club.dnd5.portal.exception.PageNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
-public class NotHandlerControllerAdvice extends VersionControllerAdvice {
+public class NotHandlerControllerAdvice {
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(PageNotFoundException.class)
-	public String handlePageNotFound(Exception exception) {
+	public String handlePageNotFound(Exception exception, Model model) {
 		return "spa";
 	}
 
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(NoHandlerFoundException.class)
-	public String handleNoHandlePageException(Exception exception) {
+	public String handleNoHandlePageException(Exception exception, Model model) {
 		return "spa";
 	}
 }
