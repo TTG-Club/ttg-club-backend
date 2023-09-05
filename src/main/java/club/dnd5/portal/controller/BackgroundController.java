e320f99435d237b2f97d9c32cb62d605393cf27e
Magistrus <gm.magistr@gmail.com>
2023-09-05 20:55:56 +0300

    refactoring

--- src/main/java/club/dnd5/portal/controller/BackgroundController.java
+++ src/main/java/club/dnd5/portal/controller/BackgroundController.java
@@ -2,6 +2,8 @@ package club.dnd5.portal.controller;
 
 import club.dnd5.portal.exception.PageNotFoundException;
 import club.dnd5.portal.model.background.Background;
+import club.dnd5.portal.model.background.Personalization;
+import club.dnd5.portal.model.background.PersonalizationType;
 import club.dnd5.portal.repository.datatable.BackgroundRepository;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Hidden
@@ -35,5 +42,15 @@ public class BackgroundController {
 		model.addAttribute("metaDescription", String.format("%s (%s) - предыстория персонажа по D&D 5 редакции", background.getName(), background.getEnglishName()));
 		model.addAttribute("menuTitle", "Предыстории");
 		return "spa";
+	}
+
+	@GetMapping("/backgrounds/fragment/{id}")
+	public String getBackgroundFragmentById(Model model, @PathVariable Integer id) {
+		Background background = backgroundRepository.findById(id).orElseThrow(PageNotFoundException::new);
+		model.addAttribute("background", background);
+		Map<PersonalizationType, List<Personalization>> tables = background.getPersonalizations().stream()
+			.collect(Collectors.groupingBy(Personalization::getType, () -> new EnumMap<>(PersonalizationType.class), Collectors.toList()));
+		model.addAttribute("tables", tables);
+		return "fragments/background :: view";
 	}
 }
