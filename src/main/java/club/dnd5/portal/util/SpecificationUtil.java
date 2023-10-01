18aba39ad5fdbc11f5b09ec7da3fd3cd222625ef
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2023-10-02 00:58:34 +0200

    Fix Timecast Filtering Logic
    Revised the timecast filtering logic to ensure accurate spell retrieval. Now, the timecast filter functions as expected
    
    Add combineWithOr method for Specifications
    
    Implemented the `combineWithOr` method in SpecificationUtil. This method enables the combination of a list of specifications using logical OR operations, providing flexibility in constructing complex queries.

--- src/main/java/club/dnd5/portal/util/SpecificationUtil.java
+++ src/main/java/club/dnd5/portal/util/SpecificationUtil.java
@@ -4,6 +4,7 @@ import club.dnd5.portal.dto.api.RequestApi;
 import club.dnd5.portal.dto.api.spells.SearchRequest;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -23,6 +24,7 @@ public final class SpecificationUtil {
 		}
 		return specification.or(addSpecification);
 	}
+
 	public static <T> Specification<T> getSearch(RequestApi request) {
 		if (Optional.of(request).map(RequestApi::getSearch).map(SearchRequest::getExact).orElse(false)) {
 			return (root, query, cb) -> cb.equal(
@@ -36,12 +38,24 @@ public final class SpecificationUtil {
 				.orElse("")
 				.trim()
 				.toUpperCase() + "%";
-			return  (root, query, cb) ->
+			return (root, query, cb) ->
 				cb.or(
 					cb.like(cb.upper(root.get("altName")), likeSearch),
 					cb.like(cb.upper(root.get("englishName")), likeSearch),
 					cb.like(cb.upper(root.get("name")), likeSearch)
 				);
 		}
+	}
+
+	public static <T> Specification<T> combineWithOr(List<Specification<T>> specifications) {
+		Specification<T> combinedSpecification = null;
+		for (Specification<T> specification : specifications) {
+			if (combinedSpecification == null) {
+				combinedSpecification = specification;
+			} else {
+				combinedSpecification = combinedSpecification.or(specification);
+			}
+		}
+		return combinedSpecification;
 	}
 }
