357f4b138953ede25f722517956cbde409ca009f
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2023-11-08 19:54:34 +0100

    Added a new method in Repository
    Added a new filterSettings for ArmorApiController

--- src/main/java/club/dnd5/portal/controller/api/item/ArmorApiController.java
+++ src/main/java/club/dnd5/portal/controller/api/item/ArmorApiController.java
@@ -1,12 +1,16 @@
 package club.dnd5.portal.controller.api.item;
 
+import club.dnd5.portal.dto.api.FilterApi;
+import club.dnd5.portal.dto.api.FilterValueApi;
 import club.dnd5.portal.dto.api.RequestApi;
 import club.dnd5.portal.dto.api.item.ArmorApi;
 import club.dnd5.portal.dto.api.item.ArmorDetailApi;
 import club.dnd5.portal.dto.api.item.ArmorRequestApi;
 import club.dnd5.portal.dto.api.spells.SearchRequest;
 import club.dnd5.portal.exception.PageNotFoundException;
+import club.dnd5.portal.model.ArmorType;
 import club.dnd5.portal.model.book.Book;
+import club.dnd5.portal.model.book.TypeBook;
 import club.dnd5.portal.model.items.Armor;
 import club.dnd5.portal.model.splells.Spell;
 import club.dnd5.portal.repository.datatable.ArmorRepository;
@@ -25,6 +29,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.JoinType;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -63,5 +69,48 @@ public class ArmorApiController {
 	@PostMapping(value = "/api/v1/armors/{englishName}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ArmorDetailApi getOption(@PathVariable String englishName) {
 		return new ArmorDetailApi(armorRepository.findByEnglishName(englishName.replace('_', ' ')).orElseThrow(PageNotFoundException::new));
+	}
+
+	@Operation(summary = "Получение фильтра для снаряжение")
+	@PostMapping("/api/v1/filters/armors")
+	public FilterApi getArmorFilter() {
+		FilterApi filters = new FilterApi();
+		List<FilterApi> sources = new ArrayList<>();
+		for (TypeBook typeBook : TypeBook.values()) {
+			List<Book> books = armorRepository.findBook(typeBook);
+			if (!books.isEmpty()) {
+				FilterApi filter = new FilterApi(typeBook.getName(), typeBook.name());
+				filter.setValues(books.stream()
+					.map(book -> new FilterValueApi(book.getSource(), book.getSource(),	Boolean.TRUE, book.getName()))
+					.collect(Collectors.toList()));
+				sources.add(filter);
+			}
+		}
+		filters.setSources(sources);
+
+		List<FilterApi> otherFilters = new ArrayList<>();
+
+		FilterApi typeFilter = new FilterApi("Тип брони", "type");
+		typeFilter.setValues(
+			Arrays.stream(ArmorType.values())
+				.map(value -> new FilterValueApi(value.getCyrillicName(), value.name()))
+				.collect(Collectors.toList()));
+		otherFilters.add(typeFilter);
+
+		FilterApi strengthFilter = new FilterApi("Требование к силе", "strength requirements");
+		List<Armor> armorList = armorRepository.findAll();
+		List<FilterValueApi> values = new ArrayList<>();
+		values.add(new FilterValueApi("Нет требований", "no requirements"));
+		for (Armor armor : armorList) {
+			//потому что в бд по стандарту null, а не 0
+			if (armor.getForceRequirements() != null) {
+				int requirements = armor.getForceRequirements();
+				values.add(new FilterValueApi(requirements + " силы", armor + " strength"));
+			}
+		}
+		strengthFilter.setValues(values);
+		otherFilters.add(strengthFilter);
+
+		return null;
 	}
 }
