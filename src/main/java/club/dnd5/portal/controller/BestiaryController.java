c8e10ea4c392c67dba750172692cff425de7ccb4
Magistrus <gm.magistr@gmail.com>
2023-11-17 10:17:52 +0200

    fix bestiary meta image by token

--- src/main/java/club/dnd5/portal/controller/BestiaryController.java
+++ src/main/java/club/dnd5/portal/controller/BestiaryController.java
@@ -2,8 +2,8 @@ package club.dnd5.portal.controller;
 
 import club.dnd5.portal.exception.PageNotFoundException;
 import club.dnd5.portal.model.creature.Creature;
-import club.dnd5.portal.model.image.ImageType;
-import club.dnd5.portal.repository.ImageRepository;
+import club.dnd5.portal.model.token.Token;
+import club.dnd5.portal.repository.TokenRepository;
 import club.dnd5.portal.repository.datatable.BestiaryRepository;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +21,7 @@ public class BestiaryController {
 	private static final String BASE_URL = "https://ttg.club/bestiary";
 
 	private final BestiaryRepository bestiaryRepository;
-	private final ImageRepository imageRepo;
+	private final TokenRepository tokenRepository;
 
 	@GetMapping("/bestiary")
 	public String getCreatures(Model model) {
@@ -37,10 +37,20 @@ public class BestiaryController {
 		Creature beast = bestiaryRepository.findByEnglishName(name.replace("_", " ")).orElseThrow(PageNotFoundException::new);
 		model.addAttribute("metaTitle", String.format("%s (%s) | Бестиарий D&D 5e", beast.getName(), beast.getEnglishName()));
 		model.addAttribute("metaUrl", String.format("%s/%s", BASE_URL, beast.getUrlName()));
-		model.addAttribute("metaDescription", String.format("%s (%s) - %s %s, %s с уровнем опасности %s", beast.getName(), beast.getEnglishName(), beast.getSizeName(), beast.getType().getCyrillicName(), beast.getAligment(), beast.getChallengeRating()));
-		Collection<String> images = imageRepo.findAllByTypeAndRefId(ImageType.CREATURE, beast.getId());
+		model.addAttribute("metaDescription", String.format("%s (%s) - %s %s, %s с уровнем опасности %s",
+				beast.getName(),
+				beast.getEnglishName(),
+				beast.getSizeName(),
+				beast.getType().getCyrillicName(),
+				beast.getAligment(),
+				beast.getChallengeRating()));
+		Collection<Token> images = tokenRepository.findByRefIdAndType(beast.getId(), "круглый");
 		if (!images.isEmpty()) {
-			model.addAttribute("metaImage", images.iterator().next());
+			model.addAttribute("metaImage", images
+					.stream()
+					.map(Token::getUrl)
+					.findFirst()
+					.orElse(""));
 		}
 		model.addAttribute("menuTitle", "Бестиарий");
 		return "spa";
