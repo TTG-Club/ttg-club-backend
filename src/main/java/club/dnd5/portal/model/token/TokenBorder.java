03d1a2d6ef32eb525954e0dd60f9cd979633aff5
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-01-18 16:34:21 +0100

    Entity TokenBorder created

---
+++ src/main/java/club/dnd5/portal/model/token/TokenBorder.java
@@ -0,0 +1,29 @@
+package club.dnd5.portal.model.token;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(
+	name = "token_borders",
+	indexes = {
+		@Index(name = "token_border_name_index", columnList = "name"),
+		@Index(name = "token_border_type_url_index", columnList = "type, url", unique = true)
+	}
+)
+public class TokenBorder {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String type;
+
+	@Column(nullable = false, unique = true)
+	private String url;
+}
