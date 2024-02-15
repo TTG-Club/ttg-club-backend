8805dcfb6954b74152481396b5ba37cfd3daa50e
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-02-15 20:00:38 +0100

    Refactor EmailService

--- src/main/java/club/dnd5/portal/service/EmailService.java
+++ src/main/java/club/dnd5/portal/service/EmailService.java
@@ -1,14 +1,18 @@
 package club.dnd5.portal.service;
 
+import club.dnd5.portal.exception.ApiException;
 import club.dnd5.portal.model.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -16,60 +20,64 @@ import java.util.UUID;
 public class EmailService {
 	private final Environment environment;
 	@Qualifier("userServiceImpl")
-	private final UserService service;
-
+	private final UserService userService;
 	private final JavaMailSender mailSender;
+
+	@Async
+	public void sendInvitationLink(List<User> users, String invitationLink) {
+		String subject = "Приглашение в группу на ttg.club";
+		String message = "Вас пригласили в группу на ttg.club. Пожалуйста, перейдите по ссылке для подтверждения:";
+		sendEmailToUsers(users, subject, message, invitationLink);
+	}
 
 	@Async
 	public void confirmRegistration(User user) {
 		String token = UUID.randomUUID().toString();
-		service.createVerificationToken(user, token);
+		userService.createVerificationToken(user, token);
 
 		String recipientAddress = user.getEmail();
 		String subject = "Подтверждение регистрации";
-		String confirmationUrl = createConfirmationUrl(token);
-		String message = "Подтвердите ваш email адрес, перейдя по ссылке:";
-
+		String confirmationUrl = "https://dev.ttg.club/confirm/email?token=" + token;
+		String message = "Подтвердите ваш email адрес, перейдя по ссылке::";
 		sendEmail(recipientAddress, subject, message, confirmationUrl);
 	}
 
 	@Async
 	public void changePassword(User user) {
 		String token = UUID.randomUUID().toString();
-		service.createVerificationToken(user, token);
+		userService.createVerificationToken(user, token);
 
 		String recipientAddress = user.getEmail();
 		String subject = "Сброс пароля на ttg.club";
-		String confirmationUrl = createPasswordResetUrl(token);
-		String message = "Для сброса пароля перейдите по ссылке и введите новый пароль:";
+		String[] profiles = this.environment.getActiveProfiles();
+		String confirmationUrl;
+		if ("dev".equals(profiles[0])) {
+			confirmationUrl = "https://dev.ttg.club/reset/password?token=" + token;
+		} else {
+			confirmationUrl = "https://ttg.club/reset/password?token=" + token;
+		}
 
+		String message = "Для сброса пароля перейдите по ссылке и введите новый пароль:";
 		sendEmail(recipientAddress, subject, message, confirmationUrl);
 	}
 
-	private String createConfirmationUrl(String token) {
-		String[] profiles = environment.getActiveProfiles();
-		if (profiles[0].equals("dev")) {
-			return "https://dev.ttg.club/confirm/email?token=" + token;
-		} else {
-			return "https://ttg.club/confirm/email?token=" + token;
+	private void sendEmailToUsers(List<User> users, String subject, String message, String url) {
+		for (User user : users) {
+			String recipientAddress = user.getEmail();
+			sendEmail(recipientAddress, subject, message, url);
 		}
 	}
 
-	private String createPasswordResetUrl(String token) {
-		String[] profiles = environment.getActiveProfiles();
-		if (profiles[0].equals("dev")) {
-			return "https://dev.ttg.club/reset/password?token=" + token;
-		} else {
-			return "https://ttg.club/reset/password?token=" + token;
-		}
-	}
-
-	private void sendEmail(String recipientAddress, String subject, String message, String confirmationUrl) {
+	private void sendEmail(String recipientAddress, String subject, String message, String url) {
 		SimpleMailMessage email = new SimpleMailMessage();
 		email.setTo(recipientAddress);
 		email.setFrom("support@ttg.club");
 		email.setSubject(subject);
-		email.setText(String.format("%s %s", message, confirmationUrl));
-		mailSender.send(email);
+		email.setText(String.format("%s %s", message, url));
+		try {
+			mailSender.send(email);
+		} catch (MailException e) {
+			throw new ApiException(HttpStatus.BAD_REQUEST, "Error sending email");
+		}
 	}
 }
