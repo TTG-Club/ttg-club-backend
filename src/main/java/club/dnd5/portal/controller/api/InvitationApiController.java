5b79e2a44a14f29745bc04a8428b53f74b35867a
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-02-27 14:13:05 +0100

    Adding a documentation for Swagger

--- src/main/java/club/dnd5/portal/controller/api/InvitationApiController.java
+++ src/main/java/club/dnd5/portal/controller/api/InvitationApiController.java
@@ -2,6 +2,7 @@ package club.dnd5.portal.controller.api;
 
 import club.dnd5.portal.dto.api.InvitationApi;
 import club.dnd5.portal.service.InvitationServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,27 +13,32 @@ import org.springframework.web.bind.annotation.*;
 public class InvitationApiController {
 	private final InvitationServiceImpl invitationService;
 
+	@Operation(summary = "Получение информации о приглашение")
 	@GetMapping("/{groupId}")
 	public InvitationApi getInvitationByGroupId(@PathVariable Long groupId) {
 		return invitationService.getInvitationByGroupId(groupId);
 	}
 
+	@Operation(summary = "Отмена приглашения")
 	@PostMapping("/{groupId}/cancel")
 	@ResponseStatus(HttpStatus.OK)
 	public void cancelInvitation(@PathVariable Long groupId) {
 		invitationService.cancelInvitation(groupId);
 	}
 
+	@Operation(summary = "Получение ссылки")
 	@GetMapping("/link/{groupId}")
 	public String getInvitationLinkByGroupId(@PathVariable Long groupId) {
 		return invitationService.getInvitationLinkByGroupId(groupId);
 	}
 
+	@Operation(summary = "Получение кода")
 	@GetMapping("/code/{groupId}")
 	public String getInvitationCodeByGroupId(@PathVariable Long groupId) {
 		return invitationService.getInvitationCodeByGroupId(groupId);
 	}
 
+	@Operation(summary = "Установка значение сколько будет валидна ссылка / code")
 	@PutMapping("/{groupId}/expiration/{days}")
 	@ResponseStatus(HttpStatus.OK)
 	public void setInvitationExpiration(@PathVariable Long groupId, @PathVariable int days) {
--- src/main/java/club/dnd5/portal/controller/api/UserPartyApiController.java
+++ src/main/java/club/dnd5/portal/controller/api/UserPartyApiController.java
@@ -2,6 +2,7 @@ package club.dnd5.portal.controller.api;
 
 import club.dnd5.portal.dto.api.UserPartyApi;
 import club.dnd5.portal.service.UserPartyService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,42 +16,49 @@ public class UserPartyApiController {
 
 	private final UserPartyService userPartyService;
 
+	@Operation(summary = "Получение группы по айди")
 	@GetMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public UserPartyApi getUserPartyById(@PathVariable Long id) {
 		return userPartyService.getUserPartyById(id);
 	}
 
+	@Operation(summary = "Получение группы по имени")
 	@GetMapping("/by-name/{name}")
 	@ResponseStatus(HttpStatus.OK)
 	public UserPartyApi getUserPartyByName(@PathVariable String name) {
 		return userPartyService.getUserPartyByName(name);
 	}
 
+	@Operation(summary = "Создание группы")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public UserPartyApi createUserParty(@RequestBody UserPartyApi userPartyDTO) {
 		return userPartyService.createUserParty(userPartyDTO);
 	}
 
+	@Operation(summary = "Обновление группы")
 	@PutMapping("/{partyId}")
 	@ResponseStatus(HttpStatus.OK)
 	public void updateUserParty(@PathVariable Long partyId, @RequestBody UserPartyApi userPartyDTO) {
 		userPartyService.updateUserParty(partyId, userPartyDTO);
 	}
 
+	@Operation(summary = "Удаление группы")
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public String deleteUserParty(@PathVariable Long id) {
 		return userPartyService.deleteUserPartyById(id);
 	}
 
+	@Operation(summary = "Получение айди юзеров, которые находятся в группе")
 	@GetMapping("/members/{partyId}")
 	@ResponseStatus(HttpStatus.OK)
 	public List<Long> getUserPartyMembers(@PathVariable Long partyId) {
 		return userPartyService.getUserPartyMembers(partyId);
 	}
 
+	@Operation(summary = "Получение всех групп")
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
 	public List<UserPartyApi> getAllUserParties() {
