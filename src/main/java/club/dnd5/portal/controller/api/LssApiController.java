7653d55e5d81f15c23b9e84d42ba420c2d435258
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-03-18 23:04:48 +0100

    Исправление json-а под формат LSS

--- src/main/java/club/dnd5/portal/controller/api/LssApiController.java
+++ src/main/java/club/dnd5/portal/controller/api/LssApiController.java
@@ -5,7 +5,6 @@ import club.dnd5.portal.service.LssService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,9 +14,8 @@ import java.util.List;
 public class LssApiController {
 	private final LssService lssService;
 
-	//@ResponseBody чтобы возвращало не html, a json
-	@GetMapping("spell/{englishName}")
-	public @ResponseBody String findByName(@PathVariable String englishName) {
+	@GetMapping(value = "spell/{englishName}", produces="application/json")
+	public String findByName(@PathVariable String englishName) {
 		return lssService.findByName(englishName);
 	}
 
