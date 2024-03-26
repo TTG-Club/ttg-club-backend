de6184b05710e2b4f3f30f624be3b596de28a4fb
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-03-26 12:57:35 +0100

    Исправление Enum под использование конструктора, а не switch

--- src/main/java/club/dnd5/portal/model/FoundryVersion.java
+++ src/main/java/club/dnd5/portal/model/FoundryVersion.java
@@ -1,17 +1,14 @@
 package club.dnd5.portal.model;
 
-public enum FoundryVersion {
-	V10,
-	V11;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-	public int getValue() {
-		switch (this) {
-			case V10:
-				return 10;
-			case V11:
-				return 11;
-			default:
-				throw new IllegalArgumentException("Unexpected enum constant: " + this);
-		}
-	}
+@RequiredArgsConstructor
+public enum FoundryVersion {
+	V10(10),
+	V11(11);
+
+	@Getter
+	private final int value;
 }
+
