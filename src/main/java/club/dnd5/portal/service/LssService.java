4d06364c2a05263ac9b4518294a92650cc7857a0
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-03-29 21:20:26 +0100

    1) Исправление пути в контроллере
    2) Обновление сервиса. Так как версия фаундри не используется по назначению там.

--- src/main/java/club/dnd5/portal/service/LssService.java
+++ src/main/java/club/dnd5/portal/service/LssService.java
@@ -1,10 +1,5 @@
 package club.dnd5.portal.service;
 
-import club.dnd5.portal.model.FoundryVersion;
-
-import java.util.List;
-
 public interface LssService {
-	String findByIdAndFoundryVersion(Integer spellId, FoundryVersion foundryVersion);
-	List<String> getAllSpellForLSS();
+	String findById(Integer spellId);
 }
