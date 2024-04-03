1740df94802d61049295aa0f53273cb61805d817
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-04-03 09:23:10 +0200

    Исправление ошибки, под более подходящею

--- src/main/java/club/dnd5/portal/controller/api/LssApiController.java
+++ src/main/java/club/dnd5/portal/controller/api/LssApiController.java
@@ -6,9 +6,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/lss")
+@RestController
+@RequestMapping(value = "/api/v1/lss")
 @Tag(name = "API для интеграции с LSS", description = "LSS Integration API")
 @RequiredArgsConstructor
 public class LssApiController {
