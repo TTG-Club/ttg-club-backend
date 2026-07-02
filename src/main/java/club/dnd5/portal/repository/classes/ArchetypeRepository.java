09cbd4362b170eee99a4a19af39da99d91fec266
Vitalii Ungurean <gm.magistr@gmail.com>
2026-07-02 09:38:18 +0300

    Добавить API редактирования архетипов

--- src/main/java/club/dnd5/portal/repository/classes/ArchetypeRepository.java
+++ src/main/java/club/dnd5/portal/repository/classes/ArchetypeRepository.java
@@ -1,6 +1,7 @@
 package club.dnd5.portal.repository.classes;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +12,7 @@ import club.dnd5.portal.model.book.TypeBook;
 import club.dnd5.portal.model.classes.archetype.Archetype;
 
 public interface ArchetypeRepository extends JpaRepository<Archetype, Integer>{
+	Optional<Archetype> findByHeroClassIdAndEnglishNameIgnoreCase(Integer classId, String englishName);
 	@Query("SELECT c.book FROM Archetype c GROUP BY c.book HAVING c.book.type = :type ORDER BY c.book.year")
 	List<Book> findBook(@Param("type") TypeBook type);
 }
